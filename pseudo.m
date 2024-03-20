0bbac4860c85dd142bc176e8e16752da1cfbee27
adi-c23 <164243695+adi-c23@users.noreply.github.com>
2024-03-20 15:38:31 -0400

    Add files via upload

---
+++ pseudo.m
@@ -0,0 +1,10 @@
+%constants
+
+
+for A=[4,9,16,25,36,49,64,81,100] %iterating over plate areas 
+    %for q=[] %iterate over charge values 
+        %for H=[] %iterate over different heights 
+            %for d=[] %iterate over different diameters 
+    
+
+end 
